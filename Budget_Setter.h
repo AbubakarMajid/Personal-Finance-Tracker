c272f1c598a240b8f7a24fd396c153b9908b72d0
AbubakarMajid <abubakarmajid036@gmail.com>
2024-05-04 20:54:56 +0500

    Budget Bug resolved

--- Budget_Setter.h
+++ Budget_Setter.h
@@ -644,7 +644,7 @@ private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e
 		}
 
 		else {
-
+			reader->Close();
 			if (total_budget <= this->income)
 			{
 				if (utility_budget != "") {
